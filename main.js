9f3eebcbe02b408b386c07dcd06d4e039e3a46a1
Matias Ignacio Paredes Marquez <66745824+Matignaciom@users.noreply.github.com>
2023-10-20 16:02:03 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+Ejemplo de cómo agregar trabajadores
+
+agregarTrabajador('Juan', 'Programador', new Date(), new Date(), 30);
+agregarTrabajador('María', 'Diseñadora', new Date(), new Date(), 45);
+*/
+
+// Agregar más trabajadores según sea necesario
